67877c77eb8974d70aafbf74cfdaf65e47825360
Au-venturous-Buddy <130553802+Au-venturous-Buddy@users.noreply.github.com>
2025-01-06 14:58:29 +0800

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.i1.v1.mbwp.v2.znzn',
-  appName: "New Year, New Bora!",
+  appName: "Zene and Zeanne Beat Energy Gap",
   webDir: 'public',
   bundledWebRuntime: false
 };
